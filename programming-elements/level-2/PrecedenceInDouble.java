9046a3e5e5f0dca5c538e10302ce2e9bc35ab3e1
ravneet <ravneet1805.be21@chitkara.edu.in>
2025-02-14 10:50:39 +0530

    level 2 question 7 and 8 completed

---
+++ programming-elements/level-2/PrecedenceInDouble.java
@@ -0,0 +1,19 @@
+//Similarly, write the DoubleOpt program by taking double values and doing the same operations.
+
+import java.util.Scanner;
+public class PrecedenceInDouble {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double c = in.nextDouble();
+
+        double result1 = a + b * c;
+        double result2 = a * b + c;
+        double result3 = c + a / b;
+        double result4 = a % b + c;
+
+        System.out.println("The results of Double Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+        in.close();
+    }
+}
---
+++ programming-elements/level-2/PrecedenceInInt.java
@@ -0,0 +1,21 @@
+//Write an IntOperation program by taking a, b, and c as input values and print the following integer operations a + b *c, a * b + c, c + a / b, and a % b + c. Please also understand the precedence of the operators.
+
+import java.util.Scanner;
+public class PrecedenceInInt {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        
+        int result1 = a + b * c;
+        int result2 = a * b + c;
+        int result3 = c + a / b;
+        int result4 = a % b + c;
+
+        System.out.println("The results of Int Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+        in.close();
+    }
+}
+
+
